478610fdfac25d84ee7cce1a66f9e25e6e98c561
Dominic <DominicArda@icloud.com>
2025-05-31 21:11:09 +0100

    #9 Create login page with route to mock dashboard

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Login Page</h1>
+
+<a href="/demos/dashboard">See a Demo?</a>
