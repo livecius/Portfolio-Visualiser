413f546bb996cac990f4b91b585db105349a4d07
Dominic <DominicArda@icloud.com>
2025-06-03 22:47:25 +0100

    #9 Add background image to login page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,24 +1,36 @@
-<div class="hero min-h-screen">
-    <div class="hero-content">
-        <div class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-            <div class="card-body">
-                <p class="text-3xl font-extrabold dark:text-white text-center">Portfolio Visualiser</p>
+<style>
+    .background {
+        background-image: url("warrenbogle.png");
+        background-size: cover;
+        background-position: center;
+        height: 100vh;
+        width: 100%;
+    }
+</style>
 
-                <fieldset class="fieldset">
-                    <label class="label">Email</label>
-                    <input type="email" class="input" placeholder="Email" />
-                    <label class="label">Password</label>
-                    <input type="password" class="input" placeholder="Password" />
-                    <div><a class="link link-hover">Forgot Password?</a></div>
-                    <button class="btn btn-outline mt-4">Login</button>
+<div class="background">
+    <div class="hero min-h-screen">
+        <div class="hero-content">
+            <div class="card bg-base-300 w-full max-w-sm shrink-0 shadow-2xl">
+                <div class="card-body rounded bg-transparent">
+                    <p class="text-3xl font-extrabold text-center">Portfolio Visualiser</p>
 
-                    <div class="divider">OR</div>
+                    <fieldset class="fieldset">
+                        <label class="label">Email</label>
+                        <input type="email" class="input" placeholder="Email" />
+                        <label class="label">Password</label>
+                        <input type="password" class="input" placeholder="Password" />
+                        <div><a class="link link-hover">Forgot Password?</a></div>
+                        <button class="btn btn-outline mt-4">Login</button>
 
-                    <button class="btn btn-outline btn-primary mt-2">Sign Up</button>
+                        <div class="divider">OR</div>
 
-                    <a class="btn btn-secondary btn-soft mt-2" href="/demos/dashboard">See a Demo?</a>
+                        <button class="btn btn-outline btn-primary mt-2">Sign Up</button>
 
-                </fieldset>
+                        <a class="btn btn-secondary btn-outline mt-2" href="/demos/dashboard">See a Demo?</a>
+
+                    </fieldset>
+                </div>
             </div>
         </div>
     </div>
