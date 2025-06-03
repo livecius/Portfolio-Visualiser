54a7286632e0ba2aa383c72975047113935b25c7
Dominic <DominicArda@icloud.com>
2025-06-03 16:35:31 +0100

    #9 Add Navbar to Mock Portfolio

--- frontend/src/routes/demos/dashboard/+page.svelte
+++ frontend/src/routes/demos/dashboard/+page.svelte
@@ -1,3 +1,10 @@
-<h1>Main Dashboard</h1>
-
-<a href="/">Log Out</a>
+<div class="navbar bg-base-100 shadow-sm">
+  <div class="flex-1">
+    <p class="text-xl">John's Portfolio</p>
+  </div>
+  <div class="flex-none">
+    <ul class="menu menu-horizontal px-1">
+      <li><a href="/">Log Out</a></li>
+    </ul>
+  </div>
+</div>
