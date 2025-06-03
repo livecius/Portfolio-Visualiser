8ebd9d60559375cf89791300f816646f3bdfb36e
Dominic <DominicArda@icloud.com>
2025-06-03 19:13:54 +0100

    #9 Add icons to navbar and add exit icon

--- frontend/src/routes/demos/dashboard/+page.svelte
+++ frontend/src/routes/demos/dashboard/+page.svelte
@@ -1,10 +1,30 @@
 <div class="navbar bg-base-100 shadow-sm">
-  <div class="flex-1">
-    <p class="text-xl">John's Portfolio</p>
+  <div class="navbar-start">
+    <div class="dropdown">
+      <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
+      </div>
+      <ul
+        tabindex="0"
+        class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 glass">
+        <li><a class="font-bold text-xl" href="/demos/dashboard">Dashboard</a></li>
+      </ul>
+    </div>
   </div>
-  <div class="flex-none">
+  <div class="navbar-center">
+    <p class="text-xl">Your Portfolio</p>
+  </div>
+  <div class="navbar-end">
     <ul class="menu menu-horizontal px-1">
-      <li><a href="/">Log Out</a></li>
+      <li>
+        <a class="" href="/">Log Out
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+          </svg>
+        </a>
+
+      </li>
     </ul>
   </div>
 </div>
