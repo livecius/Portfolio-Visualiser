923f10f4731d6f7d3ca1fe3cbc2559dc0c420d9b
Dominic <DominicArda@icloud.com>
2025-06-03 16:11:55 +0100

    #9 Create basic hero login page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,3 +1,25 @@
-<h1>Login Page</h1>
+<div class="hero min-h-screen">
+    <div class="hero-content">
+        <div class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <div class="card-body">
+                <p class="text-3xl font-extrabold dark:text-white text-center">Portfolio Visualiser</p>
 
-<a href="/demos/dashboard">See a Demo?</a>
+                <fieldset class="fieldset">
+                    <label class="label">Email</label>
+                    <input type="email" class="input" placeholder="Email" />
+                    <label class="label">Password</label>
+                    <input type="password" class="input" placeholder="Password" />
+                    <div><a class="link link-hover">Forgot Password?</a></div>
+                    <button class="btn btn-outline mt-4">Login</button>
+
+                    <div class="divider">OR</div>
+
+                    <button class="btn btn-outline btn-primary mt-2">Sign Up</button>
+
+                    <a class="btn btn-link btn-soft mt-2" href="/demos/dashboard">See a Demo?</a>
+
+                </fieldset>
+            </div>
+        </div>
+    </div>
+</div>
