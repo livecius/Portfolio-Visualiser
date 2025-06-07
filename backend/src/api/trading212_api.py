be8998ce37ef09dcb4701e07ef386cf9ca5e7adc
Dominic <DominicArda@icloud.com>
2025-06-07 21:24:23 +0100

    #10 Create methods for Account Data

--- backend/src/api/trading212_api.py
+++ backend/src/api/trading212_api.py
@@ -1,4 +1,6 @@
+from dev_config import TRADING212_API_KEY
 import requests
+
 
 class Trading212:
   """
@@ -8,8 +10,21 @@ class Trading212:
   """
   BASE_URL = "https://live.trading212.com"
 
-  def __init__(self, api_key):
+  def __init__(self, api_key = None):
+    if api_key is None:
+      if TRADING212_API_KEY is None:
+        raise ValueError("No API key provided for Trading212. Did you forget to add to the .env?")
+
+      api_key = TRADING212_API_KEY
+
     self.api_key = api_key
+
+  # Account Data
+  def fetch_account_data(self):
+    return self.requests("GET", "/api/v0/equity/account/cash")
+
+  def fetch_account_metadata(self):
+    return self.requests("GET", "/api/v0/equity/account/info")
 
   def requests(self, method, path, params = None):
     """
