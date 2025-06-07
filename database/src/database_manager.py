39988376e612a66a38e885ba4e727e95db3d5f31
Dominic <DominicArda@icloud.com>
2025-06-07 22:48:02 +0100

    #12 Import all Table abstract classes to DatabaseManager

--- database/src/database_manager.py
+++ database/src/database_manager.py
@@ -1,6 +1,6 @@
 import os
 import sqlite3
-from tables import user
+from tables import user, broker, api_keys, portfolio
 
 class DatabaseManager:
   """
@@ -19,6 +19,9 @@ class DatabaseManager:
     self.connection = sqlite3.connect(database_path)
 
     self.User = user.User(self.connection)
+    self.Broker = broker.Broker(self.connection)
+    self.ApiKeys = api_keys.ApiKeys(self.connection)
+    self.Portfolio = portfolio.Portfolio(self.connection)
 
   def add_user(self, email, password):
     self.User.add_user(email, password)
