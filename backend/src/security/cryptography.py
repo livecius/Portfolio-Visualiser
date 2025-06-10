8c7b5a0c9cdbf6f09bb85ef78b504780f9e6d932
Dominic <DominicArda@icloud.com>
2025-06-10 22:02:59 +0100

    #13 Create Hashing class with hashing method and verification

---
+++ backend/src/security/cryptography.py
@@ -0,0 +1,34 @@
+import bcrypt
+
+class Hashing:
+    @staticmethod
+    def hash(input):
+        """
+        Create a hash of the input using bcrypt.
+
+        Args:
+            input (str): The input to be hashed.
+
+        Returns:
+            bytes: The hashed input.
+        """
+
+        # bcrypt stores the salt alongside the hashed value
+        input_bytes = input.encode('utf-8')
+        input_hash = bcrypt.hashpw(input_bytes, bcrypt.gensalt())
+
+        return input_hash
+
+    @staticmethod
+    def verify_hash(input, hash):
+        """
+        Confirm the hash of the input matches the given hash.
+
+        Args:
+            input (str): The challenge input.
+
+        Returns:
+            bool: Whether the input challenge hash matches the given hash.
+        """
+        input_bytes = input.encode('utf-8')
+        return bcrypt.checkpw(input_bytes, hash)
