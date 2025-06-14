a058453c12901a150049d313461a6a8f7b7ad5ae
Dominic <DominicArda@icloud.com>
2025-06-14 21:09:04 +0100

    #18 Implement insert method for User table and add fernet_salt attribute

--- database/src/tables/user.py
+++ database/src/tables/user.py
@@ -12,6 +12,22 @@ class User(AbstractTable):
     CREATE TABLE IF NOT EXISTS user (
       user_id INTEGER PRIMARY KEY AUTOINCREMENT,
       email TEXT NOT NULL UNIQUE,
-      password TEXT NOT NULL
+      password TEXT NOT NULL,
+      fernet_salt TEXT
     );
     """)
+
+  def insert(self, email, password, fernet_salt = None):
+    """
+    Insert a row into the database.
+
+    Args:
+      email (str): The email address of the user.
+      password (bytes || str): The password for the user.
+      fernet_salt (str): The salt used in the fernet key creation.
+    """
+    insert_statement = """
+    INSERT INTO user (email, password, fernet_salt) VALUES (?, ?, ?)
+    """
+    self.cursor.execute(insert_statement, (email, password, fernet_salt))
+    self.connection.commit()
