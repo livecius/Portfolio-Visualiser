c5567aa28224ddba16af90a6aa217553fae0602d
Dominic <DominicArda@icloud.com>
2025-06-14 18:58:29 +0100

    #13 Add Encryption class to cryptography_services.py, add methods for creating fernet key from password string, encrypting and decrypting

--- backend/src/security/cryptography_services.py
+++ backend/src/security/cryptography_services.py
@@ -39,3 +39,68 @@ class Hashing:
 
 class Encryption:
   """Interface class to handle the symmetric encryption requirements."""
+
+  @staticmethod
+  def get_fernet_key_from_password(password, salt = None):
+    """
+    Generate a valid key from a password.
+
+    Args:
+      password (str): The password to be converted.
+      salt (bytes): Optional salt for fernet key creation. Default is created by bcrypt
+
+    Returns:
+      fernet_key (bytes): The generated key.
+      salt (bytes): The salt used for fernet key creation.
+    """
+    if salt is None:
+      salt = bcrypt.gensalt()
+
+    password_bytes = password.encode('utf-8')
+    hashed_password = bcrypt.hashpw(password_bytes, salt)
+
+    # Use [7:39] as the first 6 indexes are reserved for
+    # algorithm identifier and number of rounds separated by $ symbols
+    fernet_key = base64.urlsafe_b64encode(hashed_password[7:39])
+
+    return fernet_key, salt
+
+  @staticmethod
+  def encrypt(plaintext, password, salt = None):
+    """
+    Encrypt the given plaintext using fernet symmetric encryption.
+
+    Args:
+      password (str): The password key.
+      plaintext (str): The plaintext to be encrypted.
+      salt (bytes): Optional salt for fernet key creation. Default is created by bcrypt
+
+    Returns:
+      ciphertext (bytes): The ciphertext.
+      salt (bytes): The salt used for fernet key creation.
+    """
+    fernet_key, salt = Encryption.get_fernet_key_from_password(password, salt)
+    fernet_object = fernet.Fernet(fernet_key)
+
+    plaintext_bytes = plaintext.encode('utf-8')
+    ciphertext_bytes = fernet_object.encrypt(plaintext_bytes)
+
+    return ciphertext_bytes, salt
+
+  @staticmethod
+  def decrypt(ciphertext, password, salt):
+    """
+    Decrypt the given ciphertext using fernet symmetric encryption.
+
+    Args:
+      ciphertext (bytes): The ciphertext to be decrypted.
+      password (str): The password key.
+      salt (bytes): The salt used for fernet key creation.
+
+    Returns:
+      string: Decrypted decoded ciphertext.
+    """
+    fernet_key, _ = Encryption.get_fernet_key_from_password(password, salt)
+    fernet_object = fernet.Fernet(fernet_key)
+
+    return fernet_object.decrypt(ciphertext).decode('utf-8')
